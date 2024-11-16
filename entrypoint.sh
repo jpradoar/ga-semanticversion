701e5f071b24f1d777daa92d6e94e1d8e82bb9be
Jonathan Prado <jpradoar@gmail.com>
2024-11-16 21:20:10 +0100

    feat: Add more conditionals
    - I added more options in each conditionals.
    - I add new conditional (trash) to tag images that will be delete in the future.
    - I fixed some conditional evaluations to use multiple strings.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,20 +12,26 @@ base_version=$(echo $X.$Y.$Z)
 cusotm_version=$(echo $COMMIT_MSG|cut -d':' -f1)
 
 # Create new version using type (major,minor,patch,beta,none)
-if [[ $COMMIT_MSG == *major:* ]]; then
+if [[ "$COMMIT_MSG"  =~ ^(major:|breaking:|release:|big-feature:) ]]; then
   NewVersion=$(($X+1)).0.0
-elif [[ $COMMIT_MSG == *minor:* ]]; then
+elif [[ "$COMMIT_MSG" =~ ^(minor:|enhancement:|update:|feature:) ]]; then
   NewVersion=$X.$(($Y+1)).0
-elif [[ $COMMIT_MSG == *patch:* ]] || [[ $COMMIT_MSG == *fix:* ]]; then
+elif [[ "$COMMIT_MSG" =~ (fix:|patch:) ]]; then
   NewVersion=$X.$Y.$(($Z+1))
-elif [[ $COMMIT_MSG == *test:* ]]; then
-  echo "This is a simple test. I will create a random string like $base_version-string"
-  NewVersion=$base_version-$(tr -dc a-z0-9 </dev/urandom | head -c 8 ; echo '') 
+elif [[ "$COMMIT_MSG" =~ ^test: ]]; then
+  # Generate test version string
+  echo "This is a simple test. Creating a random string like $base_version-string"
+  NewVersion=$base_version-$(tr -dc a-z0-9 </dev/urandom | head -c 8)
+elif [[ "$COMMIT_MSG" =~ ^trash: ]]; then
+  # Increment patch version and add a trash suffix with random string
+  NewVersion=$X.$Y.$(($Z+1))_trash-$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 else
-  cusotm_version=$(echo $cusotm_version|cut -d' ' -f1)
-  NewVersion=$base_version-$cusotm_version
+  # Handle custom commit types
+  custom_version=$(echo $COMMIT_MSG | cut -d':' -f1 | cut -d' ' -f1)
+  NewVersion="$base_version-$custom_version"
 fi
+
 echo "COMMIT MSG: $COMMIT_MSG"
 echo "OLD VERSION: $VERSION"
 echo "NEW_VERSION: $NewVersion"
-echo "NEW_VERSION=$NewVersion" >> "$GITHUB_OUTPUT" 
+echo "NEW_VERSION=$NewVersion" >> "$GITHUB_OUTPUT"
